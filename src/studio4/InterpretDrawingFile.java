86fd1c2e5467a32060714877fba2e7363f1dda10
tennydibooglu <a.dibooglu@wustl.edu>
2025-10-01 14:10:46 -0500

    Finished1

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -2,6 +2,7 @@ package studio4;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Parameter;
 import java.util.Scanner;
 
 import javax.swing.JFileChooser;
@@ -18,19 +19,57 @@ public class InterpretDrawingFile {
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
-		Scanner in = new Scanner(f); //making Scanner with a File
-		
-		while(in.hasNext()) {
-			String shapeType = in.next();
-			int redComponent = in.nextInt();
-			int greenComponent = in.nextInt();
-			int blueComponent = in.nextInt();
-			boolean isFilled = in.nextBoolean();
-			double parameterOne = in.nextDouble();
-			double parameterTwo = in.nextDouble();
-			double parameterThreein = in.nextDouble();
+		Scanner in = new Scanner(f); // making Scanner with a File
+
+		String shapeType;
+		int redComponent;
+		int greenComponent;
+		int blueComponent;
+		boolean isFilled;
+		double penRadius = .05;
+		double parameterOne;
+		double parameterTwo;
+		double parameterThreein;
+		double parameterFour;
+		double parameterFive;
+		double parameterSix;
+		while (in.hasNext()) {
+			shapeType = in.next();
+			redComponent = in.nextInt();
+			greenComponent = in.nextInt();
+			blueComponent = in.nextInt();
+			isFilled = in.nextBoolean();
+			parameterOne = in.nextDouble();
+			parameterTwo = in.nextDouble();
+			parameterThreein = in.nextDouble();
+			StdDraw.setPenRadius(penRadius);
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			if (!isFilled) {
+				if (shapeType.equalsIgnoreCase("circle")) {
+					StdDraw.circle(parameterOne, parameterTwo, parameterThreein);
+				} else if (shapeType.equalsIgnoreCase("square")) {
+					StdDraw.square(parameterOne, parameterTwo, parameterThreein);
+				} else if (shapeType.equalsIgnoreCase("ellipse")){
+					parameterFour = in.nextDouble();
+					StdDraw.ellipse(parameterOne, parameterTwo, parameterThreein, parameterFour);
+				} else {
+					parameterFour = in.nextDouble();
+					StdDraw.rectangle(parameterOne, parameterTwo, parameterThreein, parameterFour);
+				}
+			} else {
+				if (shapeType.equalsIgnoreCase("circle")) {
+					StdDraw.filledCircle(parameterOne, parameterTwo, parameterThreein);
+				} else if (shapeType.equalsIgnoreCase("square")) {
+					StdDraw.filledSquare(parameterOne, parameterTwo, parameterThreein);
+				} else if (shapeType.equals("ellipse")) {
+					parameterFour = in.nextDouble();
+					StdDraw.filledEllipse(parameterOne, parameterTwo, parameterThreein, parameterFour);
+				} else {
+					parameterFour = in.nextDouble();
+					StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThreein, parameterFour);
+				}
+			}
 		}
-
-
+		in.close();
 	}
 }
